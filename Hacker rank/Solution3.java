f1966f18d7abdfa8fc42dc0c5d7e8cecea378e03
IshwaryalakshmiDandabany <btechcse230754@smvec.ac.in>
2026-06-08 22:46:02 +0530

    Restore HackerRank folder

---
+++ Hacker rank/Solution3.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Solution3 {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    // Cycle Detection using Floyd's Tortoise and Hare algorithm.
